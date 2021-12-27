24c3574409410c20497c37560f51aa6d568511e7
Omzlaw <omeizaalabi@gmail.com>
2021-12-27 01:26:47 +0100

    implemented firebase utils, included ability to store authenticated users in our database

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,69 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/hompage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import {
+  auth,
+  createUserProfileDocument,
+  onSnapshot,
+} from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unSubscibeFromAuth = null;
+
+  componentDidMount() {
+    // auth.onAuthStateChanged(user => {
+    //   createUserProfileDocument(user);
+
+    // })
+    this.unSubscibeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
 
+        onSnapshot(userRef, (doc) => {
+          this.setState({
+            currentUser: {
+              id: doc.id,
+              ...doc.data(),
+            },
+          }, () => console.log(this.state.currentUser));
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth,
+        });
+      }
+    });
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={< HomePage/>}></Route>
-        <Route path="/shop" element={< ShopPage/>}></Route>
-      </Routes>
-    </div>
-  );
+  componentWillUnmount() {
+    this.unSubscibeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Routes>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route path="/shop" element={<ShopPage />}></Route>
+          <Route path="/signin" element={<SignInAndSignUpPage />}></Route>
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,90 @@
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "firebase/auth";
+import {
+    getFirestore,
+    getDoc,
+    doc,
+    onSnapshot,
+    setDoc
+} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8G_xr7ypt7_AzI7b0JOfPGiC8c00ko6k",
+    authDomain: "crwn-db-68427.firebaseapp.com",
+    projectId: "crwn-db-68427",
+    storageBucket: "crwn-db-68427.appspot.com",
+    messagingSenderId: "141668668480",
+    appId: "1:141668668480:web:7a987a464750850495fba5",
+    measurementId: "G-99KXMX4PDK"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const firestore = getFirestore();
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    // prompt: 'select account'
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+
+    const userRef = doc(firestore, `users/${userAuth.uid}`);
+    const snapshot = await getDoc(userRef);
+
+
+    if (!snapshot.exists()) {
+        const {
+            displayName,
+            email
+        } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+
+    return userRef;
+}
+
+
+export {onSnapshot, createUserWithEmailAndPassword};
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider)
+    .then((result) => {
+        return result;
+    }).catch((error) => {
+        return error;
+    });
+
+
+
+export default app;
